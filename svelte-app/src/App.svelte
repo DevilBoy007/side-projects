546ddef264259c5f55132b3f55bade05fe909895
Dylan Bakr <dylan_bakr@comcast.net>
2022-04-13 20:26:14 -0400

    commit message idk what is going on

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,34 @@
+<head>
+	title="archives"
+</head>
+<script>
+import SiteView from './SiteView.svelte'
+import TwitchFeed from './TwitchFeed.svelte'
+
+const codespot = {url:'https://www.codespot.org/',
+								title:'codespot '}
+const wiki = {url:'https://wikipedia.com/',
+							 title: 'wikipedia'}
+const wotd = {url:'https://www.merriam-webster.com/word-of-the-day/calendar',
+						 title: 'word of the day'}
+const css = {url:'https://css-tricks.com/snippets/css/a-guide-to-flexbox/',
+						 title:'css-tricks flexbox'
+}
+const channel_1 = {channel:'xqcow',
+							title: 'stream #1' }
+
+</script>
+<body style="background-color:black;justify-content: space-evenly;">
+	<div style="display:flex;overflow-x:auto;">
+		<SiteView {...wotd}/>
+		<SiteView {...codespot}/>
+		<SiteView {...wiki}/>
+		<SiteView {...css}/>
+	</div>
+	<div style="display:flex;overflow-x:auto;">
+	  <SiteView {...css}/>
+		<SiteView {...wiki}/>
+		<SiteView {...codespot}/>
+		<SiteView {...wotd}/>
+	</div>
+</body>
---
+++ svelte-app/src/TwitchFeed.svelte
@@ -0,0 +1,26 @@
+<head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Stylish&display=swap" rel="stylesheet">
+</head>
+<style>
+h1
+{
+	color:deepskyblue;
+	text-shadow: -1px 1px 2px #FFF
+}
+</style>
+<script>
+	export let title
+</script>
+<body>
+	<h1 style="color:deepskyblue;font-family:Stylish">{title}</h1>
+  <!-- Add a placeholder for the Twitch embed -->
+<div id="twitch-embed"></div>
+
+<script src="https://player.twitch.tv/js/embed/v1.js"></script>
+<script type="text/javascript">
+export let channel
+new Twitch.Player("twitch-embed", { channel: channel });
+</script>
+</body>
