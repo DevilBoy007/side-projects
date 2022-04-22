e3e478bd839332f421915448bfed8c12666b8377
Dylan Bakr <dylan_bakr@comcast.net>
2022-04-22 14:26:23 -0400

    big changes

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,34 +1,31 @@
-<head>
+<svelte:head>
 	title="archives"
-</head>
+</svelte:head>
 <script>
 import SiteView from './SiteView.svelte'
 import TwitchFeed from './TwitchFeed.svelte'
+import InputBar from './InputBar.svelte'
 
-const codespot = {url:'https://www.codespot.org/',
-								title:'codespot '}
-const wiki = {url:'https://wikipedia.com/',
-							 title: 'wikipedia'}
-const wotd = {url:'https://www.merriam-webster.com/word-of-the-day/calendar',
-						 title: 'word of the day'}
-const css = {url:'https://css-tricks.com/snippets/css/a-guide-to-flexbox/',
-						 title:'css-tricks flexbox'
-}
-const channel_1 = {channel:'xqcow',
-							title: 'stream #1' }
-
+let sites = [{url:'https://www.codespot.org/',
+								  title:'codespot '},
+								{url:'https://www.merriam-webster.com/word-of-the-day/calendar',
+									title: 'word of the day'},
+								{url:'https://css-tricks.com/snippets/css/a-guide-to-flexbox/',
+									title:'css-tricks flexbox'},
+								{url:'http://www.nationalgeographic.com', title:'Nat Geo'}]
 </script>
-<body style="background-color:black;justify-content: space-evenly;">
+<body style="background-color:#272727;justify-content: space-evenly;">
+	<InputBar on:add={(e) => sites = [...sites, e.detail]}/>
 	<div style="display:flex;overflow-x:auto;">
-		<SiteView {...wotd}/>
-		<SiteView {...codespot}/>
-		<SiteView {...wiki}/>
-		<SiteView {...css}/>
+	{#each sites as site}
+	<div style="margin-top: 20vh;">
+		<SiteView {...site}/>
+	</div>
+	{/each}
 	</div>
 	<div style="display:flex;overflow-x:auto;">
-	  <SiteView {...css}/>
-		<SiteView {...wiki}/>
-		<SiteView {...codespot}/>
-		<SiteView {...wotd}/>
+	{#each sites.reverse() as site, i (i)}
+		<SiteView {...site}/>
+	{/each}
 	</div>
 </body>
--- svelte-app/src/SiteView.svelte
+++ svelte-app/src/SiteView.svelte
@@ -1,13 +1,13 @@
 <head>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Stylish&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bebas-Neue&display=swap" rel="stylesheet">
 </head>
 <style>
 h1, a
 {
 	color:deepskyblue;
-	text-shadow: -1px 1px 4px #FFF
+	text-shadow: -1px 1px 4px #0074ff
 }
 </style>
 <script>
@@ -15,6 +15,6 @@ h1, a
 	export let title
 </script>
 <body>
-	<h1 style="color:deepskyblue;font-family:Stylish"><a href={url}>{title}</a></h1>
+	<h1 style="color:deepskyblue;font-family:Bebas Neue; padding-left: 1vw;"><a href={url}>{title}</a></h1>
 		<iframe title="site" width="300" height="200" src={url} style="background-color:grey;border-radius:12px;box-shadow: -4px 2px 12px #FFF"/>
 </body>
